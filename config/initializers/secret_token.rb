2c2934459af894d4b607d359639f3858c9836cd8
Duli-chan <dulithaz@gmail.com>
2011-11-30 08:32:53 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimeTrip::Application.config.secret_token = '5410f5bc4bfd0d219ceb8a6dd647cdd790fa63f585e70fc9c60224fb741808aca76b331daa868342a461251a85f7ee438114ee6b9a415caa9bb25df44ceb4945'
